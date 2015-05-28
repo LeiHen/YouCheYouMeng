94488a2f319e7c0a6ee69cb6c3e09b8c098aa66a
流星的泪痕 <61955365@qq.com>
2015-05-28 18:30:59 +0800

    4s 发布评论

--- js/page.js
+++ js/page.js
@@ -12,7 +12,7 @@
 function mianInit(){
     //调试 禁用
 //    alert("x");
-     document.write("<script src=\'http://172.16.1.131:81/con/assets/js/f5.js\'><\script>");
+//     document.write("<script src=\'http://172.16.1.131:81/con/assets/js/f5.js\'><\script>");
 //   document.write("<script src=\'http://192.168.2.107:81/con/assets/js/f5.js\'><\script>");
 }
 mianInit();
